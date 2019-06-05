50d544f03664805921ebf12a8c0d715a6e7d9b3e
SeigoKakinuma <seigo.k-4253@hotmail.co.jp>
2019-06-06 00:04:55 +0900

    修正4

--- index.php
+++ index.php
@@ -1,37 +1,27 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-<meta charset="UTF-8">
-<title>お問い合わせフォーム</title>
-<link rel="stylesheet" href="style.css">
-<script type="text/javascript" src="contact.js"></script>
+    <meta charset="UTF-8">
+    <title>お問い合わせフォーム</title>
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="contact.js"></script>
 </head>
+
 <body>
-<div><h1>Company Name</h1></div>
-<div><h2>お問い合わせ</h2></div>
-  <?php
-$dsn = 'pgsql:dbname=d4j3vu6k5dkt1s;host=ec2-54-235-114-242.compute-1.amazonaws.com;port=5432';
-$user = 'rqctcorramtofr';
-$pass = '0a0b29fa56efd4c1776ea414bec8d385ae1578ae9f7fbc7c700145f277311368';
+    <div>
+        <h1>Company Name</h1></div>
+    <div>
+        <h2>お問い合わせ</h2></div>
+    <?php
+    $dsn = 'pgsql:dbname=d4j3vu6k5dkt1s;host=ec2-54-235-114-242.compute-1.amazonaws.com;port=5432';
+    $user = 'rqctcorramtofr';
+    $pass = '0a0b29fa56efd4c1776ea414bec8d385ae1578ae9f7fbc7c700145f277311368';
 
 try {
   // DBに接続する
   $dbh = new PDO($dsn, $user, $pass);
 
-  // ここでクエリ実行する
-    $query_result = $dbh->query('SELECT * FROM messages');
-    foreach($query_result as $row) {
-    echo $row["user_id"] . ": " . $row["message"] . $row["speech_date"] . "<br/>";
-    }
-
-  // DBを切断する
-  $dbh = null;
-} catch (PDOException $e) {
-    // 接続にエラーが発生した場合ここに入る
-    print "DB ERROR: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
 
 <div>
 	<form action="confirm.php" method="post" name="form" onsubmit="return validate()">
@@ -75,5 +65,20 @@ try {
 		<button type="submit">確認画面へ</button>
 	</form>
 </div>
+  // ここでクエリ実行する
+    $query_result = $dbh->query('SELECT * FROM messages');
+    foreach($query_result as $row) {
+    echo $row["user_id"] . ": " . $row["message"] . $row["speech_date"] . "<br/>";
+    }
+
+  // DBを切断する
+  $dbh = null;
+} catch (PDOException $e) {
+    // 接続にエラーが発生した場合ここに入る
+    print "DB ERROR: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
 </body>
+
 </html>
