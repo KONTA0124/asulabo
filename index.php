9d481a42af75774d905fdc99232edd193e601d0a
SeigoKakinuma <seigo.k-4253@hotmail.co.jp>
2019-06-09 01:10:32 +0900

    修正6

--- index.php
+++ index.php
@@ -20,6 +20,43 @@
     </div>
 
     <div>
+        <div class="container header-area-wrap clearfix">
+            <div class="row clearfix">
+                <div class="col-md-4 clearfix">
+                    <button class="playbutton">
+                        <img src="images/drink_wine_champagne_stopper.png" />
+                    </button>
+                </div>
+                <div class="col-md-4 clearfix">
+                    <button class="playbutton">
+                        <img src="images/drink_wine_champagne_stopper.png" />
+                    </button>
+                </div>
+                <div class="col-md-4 clearfix">
+                    <button class="playbutton">
+                        <img src="images/drink_wine_champagne_stopper.png" />
+                    </button>
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-4 clearfix">
+                    <button class="playbutton">
+                        <img src="images/drink_wine_champagne_stopper.png" />
+                    </button>
+                </div>
+                <div class="col-md-4 clearfix">
+                    <button class="playbutton">
+                        <img src="images/drink_wine_champagne_stopper.png" />
+                    </button>
+                </div>
+                <div class="col-md-4 clearfix">
+                    <button class="playbutton">
+                        <img src="images/drink_wine_champagne_stopper.png" />
+                    </button>
+                </div>
+            </div>
+        </div>
+
         <form action="confirm.php" method="post" name="form" onsubmit="return validate()">
             <h1 class="contact-title">お問い合わせ 内容入力</h1>
             <p>お問い合わせ内容をご入力の上、「確認画面へ」ボタンをクリックしてください。</p>
