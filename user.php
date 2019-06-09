e86980009525a4a17ce6f2efd2d8b6c6a03ff371
SeigoKakinuma <seigo.k-4253@hotmail.co.jp>
2019-06-09 12:59:11 +0900

    修正6

--- user.php
+++ user.php
@@ -41,7 +41,7 @@ try {
   $dbh = new PDO($dsn, $user, $pass);
 
   // ここでクエリ実行する
-    $query_result = $dbh->query('SELECT * FROM members WHERE name LIKE "%田中%"');
+    $query_result = $dbh->query("SELECT * FROM members WHERE name LIKE '%田中%'");
     foreach($query_result as $row) {
         print $row["name"] ;
     }
