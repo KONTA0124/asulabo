70a8f0c3781f7bbd0936e33534b5b5532d2e3070
SeigoKakinuma <seigo.k-4253@hotmail.co.jp>
2019-06-09 12:38:57 +0900

    修正6

--- user.php
+++ user.php
@@ -3,71 +3,8 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		// フォームから送信されたデータを各変数に格納
 		$name = $_POST["name"];
-        print $name; 
-	}
-
-	// 送信ボタンが押されたら
-	if (isset($_POST["submit"])) {
-		// 送信ボタンが押された時に動作する処理をここに記述する
-        	
-		// 日本語をメールで送る場合のおまじない
-        	mb_language("ja");
-		mb_internal_encoding("UTF-8");
-		
-		//mb_send_mail("kanda.it.school.trial@gmail.com", "メール送信テスト", "メール本文");
-
-        	// 件名を変数subjectに格納
-        	$subject = "［自動送信］お問い合わせ内容の確認";
-
-        	// メール本文を変数bodyに格納
-		$body = <<< EOM
-{$name}　様
-
-お問い合わせありがとうございます。
-以下のお問い合わせ内容を、メールにて確認させていただきました。
-
-===================================================
-【 お名前 】 
-{$name}
-
-【 ふりがな 】 
-{$furigana}
-
-【 メール 】 
-{$email}
-
-【 電話番号 】 
-{$tel}
-
-【 性別 】 
-{$sex}
-
-【 項目 】 
-{$item}
-
-【 内容 】 
-{$content}
-===================================================
-
-内容を確認のうえ、回答させて頂きます。
-しばらくお待ちください。
-EOM;
-        
-		// 送信元のメールアドレスを変数fromEmailに格納
-		$fromEmail = "contact@dream-php-seminar.com";
-
-		// 送信元の名前を変数fromNameに格納
-		$fromName = "お問い合わせテスト";
-
-		// ヘッダ情報を変数headerに格納する		
-		$header = "From: " .mb_encode_mimeheader($fromName) ."<{$fromEmail}>";
-
-		// メール送信を行う
-		mb_send_mail($email, $subject, $body, $header);
-
- 		// サンクスページに画面遷移させる
-		header("Location: http://testapp.hippy.jp/contact/thanks.php");
-		exit;
+        $name = $_POST["sex"];
+        $name = $_POST["age"];
 	}
 ?>
 
