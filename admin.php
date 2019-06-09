143df0b4bea19b5958083520196b3e422a774c4a
SeigoKakinuma <seigo.k-4253@hotmail.co.jp>
2019-06-09 10:38:18 +0900

    修正6

--- admin.php
+++ admin.php
@@ -24,33 +24,12 @@
         <p>お問い合わせ内容をご入力の上、「確認画面へ」ボタンをクリックしてください。</p>
         <div>
             <div>
-                <label>お名前<span>必須</span></label>
+                <label>お名前</label>
                 <input type="text" name="name" placeholder="例）山田太郎" value="">
             </div>
             <div>
-                <label>ふりがな<span>必須</span></label>
-                <input type="text" name="furigana" placeholder="例）やまだたろう" value="">
-            </div>
-            <div>
-                <label>メールアドレス<span>必須</span></label>
-                <input type="text" name="email" placeholder="例）guest@example.com" value="">
-            </div>
-            <div>
-                <label>電話番号<span>必須</span></label>
-                <input type="text" name="tel" placeholder="例）0000000000" value="">
-            </div>
-            <div>
                 <label>性別<span>必須</span></label>
-                <input type="radio" name="sex" value="男性" checked> 男性
-                <input type="radio" name="sex" value="女性"> 女性
-            </div>
-            <div>
-                <label>お問い合わせ項目<span>必須</span></label>
-                <select name="item">
-                    <option value="">お問い合わせ項目を選択してください</option>
-                    <option value="ご質問・お問い合わせ">ご質問・お問い合わせ</option>
-                    <option value="ご意見・ご感想">ご意見・ご感想</option>
-                    <?php
+                <?php
 $dsn = 'pgsql:dbname=d4j3vu6k5dkt1s;host=ec2-54-235-114-242.compute-1.amazonaws.com;port=5432';
 $user = 'rqctcorramtofr';
 $pass = '0a0b29fa56efd4c1776ea414bec8d385ae1578ae9f7fbc7c700145f277311368';
@@ -60,9 +39,9 @@ try {
   $dbh = new PDO($dsn, $user, $pass);
 
   // ここでクエリ実行する
-    $query_result = $dbh->query('SELECT * FROM messages');
+    $query_result = $dbh->query('SELECT * FROM sexes');
     foreach($query_result as $row) {
-        print '<option value="">' . $row["message"] . '</option>';
+        print '<input type="radio" name="sex" value="' . $row["id"] . '" checked> ' . $row["name"];
     }
 
   // DBを切断する
@@ -73,6 +52,18 @@ try {
     die();
 }
 ?>
+            </div>
+            <div>
+                <label>年代<span>必須</span></label>
+                <input type="radio" name="sex" value="男性" checked> 男性
+                <input type="radio" name="sex" value="女性"> 女性
+            </div>
+            <div>
+                <label>お問い合わせ項目<span>必須</span></label>
+                <select name="item">
+                    <option value="">お問い合わせ項目を選択してください</option>
+                    <option value="ご質問・お問い合わせ">ご質問・お問い合わせ</option>
+                    <option value="ご意見・ご感想">ご意見・ご感想</option>
                 </select>
             </div>
             <div>
